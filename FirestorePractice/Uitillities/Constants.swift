1b3cb848dc68acef20d94af480de250d78a00ed5
Bernard <hurgoon@gmail.com>
2019-03-24 11:37:48 +0900

    16. Firestore Transactions

--- FirestorePractice/Uitillities/Constants.swift
+++ FirestorePractice/Uitillities/Constants.swift
@@ -11,6 +11,7 @@ import Firebase
 
 let THOUGHTS_REF = "thoughts" // Collection
 let USERS_REF = "users"
+let COMMENTS_REF = "comments"
 
 let CATEGORY = "category"
 let NUM_COMMENTS = "numComments"
@@ -20,4 +21,5 @@ let TIMESTAMP = "timestamp"
 let USERNAME = "username"
 let DATE_CREATED = "dateCreated" // User 등록 시간
 //let CURRENT_USER = Auth.auth().currentUser // 이건 쓰면 안됌...
+let COMMENT_TXT = "commentTxt"
 
